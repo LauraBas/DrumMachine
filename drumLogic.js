9cc421b71ab57414ed91550aba47e05c6bf12169
Laura Bassani <bassanilaurarita@gmail.com>
2020-06-04 16:56:38 +0200

    display the titles and add keyboard sounds

--- drumLogic.js
+++ drumLogic.js
@@ -1,8 +1,17 @@
-function Play(letra) {
-    document.getElementById(letra).play();
-    let display = document.getElementById("disp")
-    let description = document.getElementById(letra).dataset["desc"]
-    display.innerHTML = description;
 
+function onkeyPressed(arg){
+    Play(arg.key.toUpperCase());
 }
 
+function display(text) {
+    let display = document.getElementById("disp")
+    let description = document.getElementById(text).dataset["desc"]
+    display.innerHTML = description;
+}
+
+function Play(letra) {
+    display(letra);
+    document.getElementById(letra).play();
+}
+
+document.onkeypress = onkeyPressed;
