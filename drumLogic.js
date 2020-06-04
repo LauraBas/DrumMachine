f62b9707dd4bf5e1765b0fc4ed64fe97358318de
Laura Bassani <bassanilaurarita@gmail.com>
2020-06-04 16:25:54 +0200

    add data-desc html

--- drumLogic.js
+++ drumLogic.js
@@ -1,4 +1,8 @@
-
-function Play(letra){
+function Play(letra) {
     document.getElementById(letra).play();
+    let display = document.getElementById("disp")
+    let description = document.getElementById(letra).dataset["desc"]
+    display.innerHTML = description;
+
 }
+
